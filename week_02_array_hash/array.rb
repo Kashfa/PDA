cd118e8663344060720d34cc3c7eea29d73086d8
Kashfa <cyan82@hotmail.com>
2018-04-02 11:32:29 +0100

    example of hash and array

---
+++ week_02_array_hash/array.rb
@@ -0,0 +1,10 @@
+
+teams =
+["Hammerheads",
+"Great Whites",
+"Tigers",
+"Lions"]
+
+
+
+print teams.index("Tigers")
---
+++ week_02_array_hash/hashes.rb
@@ -0,0 +1,13 @@
+events =  {
+           :January => "Prom",
+           :Febuary => "Valentine",
+           :March => "Birthday"
+         }
+
+
+
+events["January"] = "Prom"
+p events
+
+events["March"] = "Birthday"
+p events
